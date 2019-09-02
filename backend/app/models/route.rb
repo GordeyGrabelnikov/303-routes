1923d2575ac0d6812551974933ca748d25c51ed2
Gordey Grabelnikov <grabelnikov-1995@mail.ru>
2019-09-02 13:00:56 +0600

    add images for model route

--- backend/app/models/route.rb
+++ backend/app/models/route.rb
@@ -1,12 +1,9 @@
 class Route < ApplicationRecord
   enum route_type: [ :walk, :bike, :car ]
-
+  belongs_to :user
   has_many :points_routes
   has_many :points, through: :points_routes
-
   has_many :events
-
-  belongs_to :user
-
   has_many :comments, as: :commentable
+  has_many_attached :route_images
 end
